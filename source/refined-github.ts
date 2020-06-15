7810d8c1d29cbf3f4a1724f560e75e5e4ffefc72
Yakov <16872793+yakov116@users.noreply.github.com>
2020-06-14 20:10:56 -0400

    Add `pr-parent-commits` feature

--- source/refined-github.ts
+++ source/refined-github.ts
@@ -181,6 +181,7 @@ import './features/prevent-pr-commit-link-loss';
 import './features/first-published-tag-for-merged-pr';
 import './features/show-associated-branch-prs-on-fork';
 import './features/faster-reviews';
+import './features/pr-parent-commits';
 
 // Add global for easier debugging
 (window as any).select = select;
